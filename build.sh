c7818cadbc3b9f15b5efb56555e9fb4a4909c0f2
Alexander Smith <asmitl@gmu.edu>
2025-08-17 03:35:15 -0400

    Build image for both arm64 and amd64 platforms.

--- build.sh
+++ build.sh
@@ -3,25 +3,18 @@
 # Abort immediately on error
 set -e
 
+# Set defaults
+image_repo=${USER}
+image_tag="build"
+platform="linux/arm64,linux/amd64"
+
+# Set build variables from conf file
 source build.conf
-
-
-if [[ -z "${image_repo}" ]]
-then
-    image_repo=${USER}
-fi
-
-if [[ -z "${image_tag}" ]]
-then
-    image_tag="build"
-fi
-
 
 image_str="${image_repo}/binftools:${image_tag}"
 
-
 build_opts="\
---force-rm -f Dockerfile --platform=linux/arm64/v8 -t ${image_str}
+--force-rm -f Dockerfile --platform=${platform} -t ${image_str}
 "
 
 cd $(dirname $0)
