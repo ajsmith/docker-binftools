d37461f080c5110e650b2cbdb093188c0e621685
Alexander Smith <asmitl@gmu.edu>
2025-08-20 00:15:04 -0400

    Install CUDA on amd64 image.

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -4,7 +4,13 @@ set -e
 
 cd $(dirname $0)
 
-dnf install -y --setopt=tsflags='' $(cat packages.txt)
 dnf clean all
+dnf update -y
+dnf install -y --setopt=tsflags='' $(cat packages.txt)
+
+if [[ "$(uname -m)" == "amd64" ]]
+then
+    dnf install -y $(cat cuda-packages.txt)
+fi
 
 mandb -c
