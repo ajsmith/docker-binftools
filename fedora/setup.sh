8bcaa7610e448192b8e52107cc8b9367b2a63342
Alexander Smith <asmitl@gmu.edu>
2025-08-20 00:37:03 -0400

    Fix arch check for installing CUDA packages.

--- fedora/setup.sh
+++ fedora/setup.sh
@@ -8,7 +8,7 @@ dnf clean all
 dnf update -y
 dnf install -y --setopt=tsflags='' $(cat packages.txt)
 
-if [[ "$(uname -m)" == "amd64" ]]
+if [[ "$(uname -m)" == "x86_64" ]]
 then
     dnf install -y $(cat cuda-packages.txt)
 fi
